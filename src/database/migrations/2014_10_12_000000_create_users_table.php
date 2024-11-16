d1d7cef0658f68d7b41a50832a153eab8428ae6c
takamiya yushi <impreza5531593@gmail.com>
2024-11-16 22:18:32 +0900

    リポジトリ更新

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name')->nullable(false);
+            $table->string('email')->nullable(false);
+            $table->string('password')->nullable(false);
             $table->timestamps();
         });
     }
